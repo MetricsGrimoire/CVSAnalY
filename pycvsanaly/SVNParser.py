2f5281cf8d758c36f3b585fc71d30b9bf755a90f
Luis Cañas Díaz <lcanas@gsyc.es>
2008-02-19 10:32:45 +0000

    pycvsanaly/SVNParser.py
    2008-02-19  Luis Cañas Díaz  <lcanas@gsyc.es>
    
            * pycvsanaly/SVNParser.py
    
            Comment token # restored in first line

--- pycvsanaly/SVNParser.py
+++ pycvsanaly/SVNParser.py
@@ -1,4 +1,4 @@
- Copyright (C) 2007 LibreSoft
+# Copyright (C) 2007 LibreSoft
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
